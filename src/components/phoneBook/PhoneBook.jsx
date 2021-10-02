5dfbfb527b2fcc575081b2934a465c8d23f96780
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-10-02 14:30:00 +0200

    Create PhoneBook.jsx

---
+++ src/components/phoneBook/PhoneBook.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './PhoneBook.module.css';
+
+class PhoneBook extends React.Component {
+  render() {
+    return <div>ffff</div>;
+  }
+}
+
+export default PhoneBook;
